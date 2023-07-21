cf58aa021c0b0e623fc30f47e39710b6b790d211
Hmxs <18321938171@163.com>
2023-07-21 19:18:25 +0700

    Delete .idea

---
+++ Assets/Hmxs/Test.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Hmxs
+{
+    public class Test : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
