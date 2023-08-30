0ae86275b7826a4fac158e60b6025723a2d9fb9a
Nianyi Wang <wangnianyi2001@outlook.com>
2023-08-31 01:00:32 +0800

    Implement a mesh tile utility

---
+++ Assets/Scripts/Utilities/MeshTile.gizmos.cs
@@ -0,0 +1,56 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+namespace NaniCore.UnityPlayground {
+	public partial class MeshTile : MonoBehaviour {
+		#region Fields
+		private const string gizmosRootName = "$MeshTileGizmosRoot";
+		private const HideFlags gizmosHideFlag = HideFlags.HideAndDontSave;
+		private Transform gizmosRoot;
+		#endregion
+
+		#region Functions
+		private GameObject InstantiateGizmos(GameObject template, Transform under) {
+			var instance = PrefabUtility.InstantiatePrefab(template, under) as GameObject;
+			instance.hideFlags = gizmosHideFlag;
+			return instance;
+		}
+		#endregion
+
+		#region Life cycle
+		private void OnValidate() {
+			if(Application.isPlaying)
+				return;
+			EditorApplication.delayCall += () => {
+				if(Application.isPlaying)
+					return;
+				if(gizmosRoot == null)
+					gizmosRoot = transform.Find(gizmosRootName);
+				if(gizmosRoot != null) {
+					DestroyImmediate(gizmosRoot.gameObject);
+					gizmosRoot = null;
+				}
+				gizmosRoot = new GameObject(gizmosRootName).transform;
+				gizmosRoot.SetParent(transform, false);
+				gizmosRoot.gameObject.hideFlags = gizmosHideFlag;
+				Construct(gizmosRoot, InstantiateGizmos);
+				gizmosRoot?.gameObject?.SetActive(enabled);
+			};
+		}
+
+		private void OnEnable() {
+			if(Application.isPlaying)
+				return;
+			gizmosRoot?.gameObject?.SetActive(true);
+		}
+
+		private void OnDisable() {
+			if(Application.isPlaying)
+				return;
+			gizmosRoot?.gameObject?.SetActive(false);
+		}
+		#endregion
+	}
+}
+#endif
