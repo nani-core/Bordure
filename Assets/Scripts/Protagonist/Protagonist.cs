44b2307f36e8116d46f4b1d6443479098b6eb85d
Nianyi Wang <wangnianyi2001@outlook.com>
2023-09-18 17:50:44 +0800

    Implement cheating

--- Assets/Scripts/Protagonist/Protagonist.cs
+++ Assets/Scripts/Protagonist/Protagonist.cs
@@ -7,6 +7,34 @@ namespace NaniCore.Loopool {
 		public static Protagonist instance;
 		#endregion
 
+		#region Functions
+		public void Cheat() {
+			if(Raycast(out RaycastHit hitInfo)) {
+				for(var target = hitInfo.transform; target != null; target = target.parent) {
+					bool acted = false;
+					foreach(var component in target.GetComponents<Component>()) {
+						switch(component) {
+							case AutomaticDoor door:
+								door.ToggleOpeningState();
+								acted = true;
+								break;
+							case PressurePlate plate:
+								plate.Pressed = !plate.Pressed;
+								acted = true;
+								break;
+							case Interactable interactable:
+								interactable.SendMessage("OnInteract", SendMessageOptions.DontRequireReceiver);
+								acted = true;
+								break;
+						}
+					}
+					if(acted)
+						break;
+				}
+			}
+		}
+		#endregion
+
 		#region Life cycle
 #if UNITY_EDITOR
 		protected void OnValidate() {
--- Assets/Scripts/Protagonist/Protagonist.interaction.cs
+++ Assets/Scripts/Protagonist/Protagonist.interaction.cs
@@ -94,14 +94,12 @@ namespace NaniCore.Loopool {
 		private void LateUpdateInteraction() {
 			// If not grabbing anything, check for focus.
 			if(GrabbingObject == null) {
-				RaycastHit hitInfo;
-				bool isHit = Physics.Raycast(camera.ViewportPointToRay(Vector2.one * .5f), out hitInfo, maxInteractionDistance);
+				bool isHit = Raycast(out RaycastHit hitInfo);
 				FocusingObject = isHit ? hitInfo.collider.GetComponent<Interactable>() : null;
 			}
 			// If grabbing blocked, drop.
 			else {
-				RaycastHit hitInfo;
-				bool isHit = Physics.Raycast(camera.ViewportPointToRay(Vector2.one * .5f), out hitInfo, maxInteractionDistance);
+				bool isHit = Raycast(out RaycastHit hitInfo);
 				// Don't drop if not hit, might be due to orienting too fast.
 				if(isHit) {
 					bool isHitPointIntertweening = Vector3.Distance(hitInfo.point, Eye.position) < Vector3.Distance(GrabbingObject.transform.position, Eye.position);
@@ -141,6 +139,10 @@ namespace NaniCore.Loopool {
 				focus.UpdateFocusAnimated(1);
 			else
 				focus.UpdateFocusAnimated(0);
+		}
+
+		private bool Raycast(out RaycastHit hitInfo) {
+			return Physics.Raycast(camera.ViewportPointToRay(Vector2.one * .5f), out hitInfo, maxInteractionDistance);
 		}
 
 		#region Grabbing
