cbd87ba98ab4c9117adfac8fca7a457a122c58fd
Nianyi Wang <wangnianyi2001@outlook.com>
2024-05-01 08:41:51 +0800

    Change collision sound rolloff mode to linear

--- Assets/Scripts/Utilities/AudioSettings.cs
+++ Assets/Scripts/Utilities/AudioSettings.cs
@@ -16,10 +16,13 @@ namespace NaniCore.Bordure {
 		[Header("Collision")]
 		[Tooltip("How much portion of the enery lost due to collision will be transfered into sound. Measured in logarithm in 10.")]
 		[NaughtyAttributes.Label("Energy Conversion Rate (log 10)")]
-		[Range(-10, 0)][SerializeField] private float collisionSoundEnergyConversionRateLog = -3f;
+		[Range(-10, 0)][SerializeField] private float collisionSoundEnergyConversionRateLog = -3.0f;
 		public float CollisionSoundEnergyConversionRate => Mathf.Pow(10, collisionSoundEnergyConversionRateLog);
+		[Range(0, 10)] public float rangeFactorA = 2.0f;
+		[Range(1, 5)] public float rangeExponentialBase = 3.0f;
+		[Range(0, 10)] public float rangeFactorB = 3.0f;
 		[Tooltip("How strongly can collision sounds be played at maximum. This is to prevent a too-loud sound from being played.")]
-		[Range(0, 10)] public float maxVolume = 1.0f;
+		[Range(0, 3)] public float maxVolume = 1.0f;
 		public List<AudioClip> defaultCollisionSounds;
 		public List<SoundSet> collisionSoundSets = new();
 
--- Assets/Scripts/Utilities/Statics/AudioUtility.cs
+++ Assets/Scripts/Utilities/Statics/AudioUtility.cs
@@ -7,13 +7,7 @@ namespace NaniCore {
 			public Vector2 range;
 			public float volume;
 			public float spatialBlend;
-
-			public AudioPlayConfig(Vector2 range, float volume, float spatialBlend) {
-				this.range = range;
-				this.volume = volume;
-				this.spatialBlend = spatialBlend;
-			}
-			public AudioPlayConfig(AudioPlayConfig config) : this(config.range, config.volume, config.spatialBlend) { }
+			public AudioRolloffMode rolloffMode;
 
 			public readonly void ApplyOn(AudioSource source) {
 				if(source == null)
@@ -24,25 +18,20 @@ namespace NaniCore {
 				source.minDistance = range.x;
 				source.maxDistance = range.y;
 				source.spatialBlend = spatialBlend;
+				source.rolloffMode = rolloffMode;
 			}
 		}
 
-		public static AudioPlayConfig defaultAudioPlayConfig = new() {
-			range = new Vector2(0, 1),
-			volume = 1f,
-			spatialBlend = 1f,
-		};
-
-		public static IEnumerator PlayOneShotAtCoroutine(AudioClip clip, Vector3 worldPosition, Transform under, float volume = 1.0f) {
+		public static IEnumerator PlayOneShotAtCoroutine(AudioClip clip, Vector3 worldPosition, Transform under, AudioPlayConfig config) {
 			if(clip == null)
 				yield break;
 
-			volume = Mathf.Abs(volume);
-			if(volume == 0.0f) {
+			config.volume = Mathf.Abs(config.volume);
+			if(config.volume == 0.0f) {
 				yield return new WaitForSeconds(clip.length);
 				yield break;
 			}
-			bool needToGainClipAtRuntime = volume > 1.0f;
+			bool needToGainClipAtRuntime = config.volume > 1.0f;
 			// If volume is greater than 1.0f, we need to create a level-gained version of the clip at runtime.
 			if(needToGainClipAtRuntime) {
 				var copy = AudioClip.Create($"{clip.name} (gained)", clip.samples, clip.channels, clip.frequency, false);
@@ -57,8 +46,8 @@ namespace NaniCore {
 					yield break;
 				}
 				for(int i = 0; i < data.Length; ++i)
-					data[i] *= volume;
-				volume = 1.0f;
+					data[i] *= config.volume;
+				config.volume = 1.0f;
 				copy.SetData(data, 0);
 				clip = copy;
 			}
@@ -69,11 +58,7 @@ namespace NaniCore {
 
 			AudioSource source = player.AddComponent<AudioSource>();
 			source.playOnAwake = false;
-			source.spatialBlend = 1.0f;
-			source.rolloffMode = AudioRolloffMode.Logarithmic;
-			source.minDistance = 1.0f;
-			source.maxDistance = 500.0f;
-			source.volume = volume;
+			config.ApplyOn(source);
 
 			source.PlayOneShot(clip);
 
@@ -89,6 +74,15 @@ namespace NaniCore {
 			if(needToGainClipAtRuntime) {
 				Bordure.GameManager.Instance.ReleaseResource(clip);
 			}
+		}
+		public static IEnumerator PlayOneShotAtCoroutine(AudioClip clip, Vector3 worldPosition, Transform under) {
+			AudioPlayConfig config = new() {
+				range = new Vector2(1.0f, 500.0f),
+				volume = 1.0f,
+				spatialBlend = 1.0f,
+				rolloffMode = AudioRolloffMode.Logarithmic,
+			};
+			return PlayOneShotAtCoroutine(clip, worldPosition, under, config);
 		}
 
 		public static float CalculateTotalEnergy(this AudioClip clip) {
