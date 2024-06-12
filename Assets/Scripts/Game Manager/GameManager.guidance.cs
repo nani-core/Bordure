49b685b17795fdd045bfb71b861b67ca6195006f
Nianyi Wang <wangnianyi2001@outlook.com>
2024-06-12 17:20:56 +0800

    Move some prefab assets

---
+++ Assets/Scripts/Game Manager/GameManager.guidance.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace NaniCore.Bordure {
+	public partial class GameManager {
+		#region Serialized fields
+		[SerializeField] private CanvasGroup guidanceRoot;
+		#endregion
+
+		#region Fields
+		#endregion
+
+		#region Properties
+		#endregion
+
+		#region Life cycle
+		#endregion
+
+		#region Functions
+		#endregion
+	}
+}
